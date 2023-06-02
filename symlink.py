0ad9e3341ad33557cfc71111e316fc5099815fd1
Kristian Stangeland <kr_stang@hotmail.com>
2023-06-03 01:51:32 +0200

    Remove test code

--- symlink.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-
-# Test symlink support
-sourcepath = pathlib.Path("F:\\Temp\\A").absolute()
-destination = pathlib.Path("F:\\Temp\\B").absolute()
-
-destination.symlink_to(sourcepath)
